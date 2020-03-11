630b93894f02f893d1e1d99beb29f61323b612b1
Jeff C <j.crawford37@gmail.com>
2020-03-11 13:23:16 -0400

    Added slash command for /fbm toggle to show and hide and /fbm reset to reset the number

--- FrostboltMeter.lua
+++ FrostboltMeter.lua
@@ -2,7 +2,31 @@
 -- 7 Mar 2020
 -- Olgruff / Hateduck fun addons for fun
 
+-- Global Saved Variables
+-- Saved when logout happens
 totalFrostbolts = 0
+
+--  slash command to toggle on and off
+SLASH_FROSTBOLTMETER1 = '/fbm'
+
+-- WoW way to add things to the command list table
+function SlashCmdList.FROSTBOLTMETER(msg, editbox) 
+		if (msg == "reset") then
+		FrostboltMeter_ResetStats()
+	elseif (msg == "toggle") then
+		if FrostboltMeterMain:IsVisible() then
+			FrostboltMeterMain:Hide()
+		else 
+			FrostboltMeterMain:Show()
+		end
+	end
+end
+
+function FrostboltMeter_ResetStats()
+	totalFrostbolts = 0
+	FrostboltCount:SetText(string.format("%09d",totalFrostbolts))
+end
+
 
 function FrostboltMeterMain_OnLoad()
 	FrostboltMeterMain:Show()
