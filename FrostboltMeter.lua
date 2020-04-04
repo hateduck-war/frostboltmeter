c51a143488bc20ab7efd2fe9b4aaae3f413e779c
Jeff C <j.crawford37@gmail.com>
2020-04-04 17:06:36 -0400

    Added Sounds, Achievement windows, Configuration Window, and 1000 bolts unlock. Will do a release when I have 2000, 4000, 8000 completed

--- FrostboltMeter.lua
+++ FrostboltMeter.lua
@@ -1,23 +1,30 @@
 -- Frostbolt Meter
--- 14 Mar 2020
+-- 14 Mar 2020 Original Commit
+-- 04 APR 2020
 -- Olgruff / Hateduck fun addons for fun
+
+-- Olgruff actual bolts 7673
+-- Lvl 31
 
 -- Global Saved Variables
 -- Saved when logout happens
--- actual 4031
 totalFrostbolts = 0
 zeroPadString = "%09d"
+achievePopUpEnable = true
+preferredFont = "NumberFontNormalLarge"
+totalFrostyCrits = 0
+totalFrostyDmg = 0
+totalPartialResist = 0
 
 -- Table to hold Achiement Nums
-
-frostyAchieveNums = {100, 500, 1000, 2000, 4000, 8000}
+frostyAchieveNums = {1000, 2000, 4000, 8000, 16000, 32000}
 
 --  slash command to toggle on and off
 SLASH_FROSTBOLTMETER1 = '/fbm'
 
 -- WoW way to add things to the command list table
 function SlashCmdList.FROSTBOLTMETER(msg, editbox) 
-		if (msg == "reset") then
+	if (msg == "reset") then
 		FrostboltMeter_ResetStats()
 	elseif (msg == "toggle") then
 		if FrostboltMeterMain:IsVisible() then
@@ -31,56 +38,73 @@ end
 
 
 function FrostboltMeterMain_OnLoad(self)
-	-- FrostboltMeterMain:Show()
+
+	-- Make these windows movable with a mouse drag
 	FrostboltMeterMain:SetScript("OnMouseDown", FrosboltMeterStartMove)
 	FrostboltMeterMain:SetScript("OnMouseUp", FrostBoltMeterStopMove)
 	FrostboltMeterMainAchievement:SetScript("OnMouseDown", FrosboltMeterStartMove)
 	FrostboltMeterMainAchievement:SetScript("OnMouseUp", FrostBoltMeterStopMove)
+	FrostboltMeterMainConfigure:SetScript("OnMouseDown", FrosboltMeterStartMove)
+	FrostboltMeterMainConfigure:SetScript("OnMouseUp", FrostBoltMeterStopMove)
+
+	--Register Events
 	FrostboltMeterMain:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
 	FrostboltMeterMain:RegisterEvent("PLAYER_ENTERING_WORLD")
+	FrostboltMeterMain:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 
 	print("Loaded Frostbolt Odometer!")
 end
 
+function FrostboltMeterConfigure_OnLoad(self)
+	if (totalFrostbolts >= 1000) then
+		FrostboltMeterMainConfigureAchievement1000:Show()
+		FrostboltMeterMainConfigureAchievement1000Locked:Hide()
+	else
+		FrostboltMeterMainConfigureAchievement1000Locked:Show()
+		FrostboltMeterMainConfigureAchievement1000:Hide()
+	end
+end
+--====================================================================================
+--		OnClick Handlers for the Buttons in FrostboltMeter.xml
+--====================================================================================
+function ConfigButton_OnClick()
+	if (FrostboltMeterMainConfigure:IsVisible()) then
+		FrostboltMeterMainConfigure:Hide()
+	else
+		FrostboltMeterMainConfigure:Show()
+	end
+end
+
 function ResetButton_OnClick()
-	if (FrostboltMeterMainResetYes:IsVisible()) then
-		ResetConfirm_Hide()
+	if (FrostboltMeterMainConfigureResetYes:IsVisible()) then
+		FrostboltMeterMainConfigureResetYes:Hide()
+		FrostboltMeterMainConfigureResetNo:Hide()
 	else
-		ResetConfirm_Show()
+		FrostboltMeterMainConfigureResetYes:Show()
+		FrostboltMeterMainConfigureResetNo:Show()		
 	end
 end
 
-function ResetConfirm_Show()
-	FrostboltMeterMainResetYes:Show()
-	FrostboltMeterMainResetNo:Show()
-	FrostboltMeterMainResetText:Show()
-end
-
-function ResetConfirm_Hide()
-	FrostboltMeterMainResetYes:Hide()
-	FrostboltMeterMainResetNo:Hide()
-	FrostboltMeterMainResetText:Hide()
-end
-
+-- Turn on zero padding if not enabled
+-- (adds extra zeros to the left)
 function ZeroPadButton_OnClick()
-	if (FrostboltMeterMainZeroPadYes:IsVisible()) then
-		ZeroPadConfirm_Hide()
+	if (zeroPadString ~= "%09d") then
+		-- Put 9 zeros in front of #
+		zeroPadString = "%09d"
 	else
-		ZeroPadConfirm_Show()
+		zeroPadString = "%d"
 	end
+	FrostboltCount:SetText(string.format(zeroPadString,totalFrostbolts))
 end
 
-
-function ZeroPadConfirm_Show()
-	FrostboltMeterMainZeroPadYes:Show()
-	FrostboltMeterMainZeroPadNo:Show()
-	FrostboltMeterMainZeroPadText:Show()
-end
-
-function ZeroPadConfirm_Hide()
-	FrostboltMeterMainZeroPadYes:Hide()
-	FrostboltMeterMainZeroPadNo:Hide()
-	FrostboltMeterMainZeroPadText:Hide()
+function ToggleAchieves_OnClick()
+	if (achievePopUpEnable == true) then
+		achievePopUpEnable = false
+		FrostboltMeterMainConfigureToggleAchievesHeader:SetText("Toggle Achievement Popups: Disabled")
+	else
+		achievePopUpEnable = true
+		FrostboltMeterMainConfigureToggleAchievesHeader:SetText("Toggle Achievement Popups: Enabled")
+	end
 end
 
 function FrostboltMeter_ResetStats()
@@ -88,77 +112,99 @@ function FrostboltMeter_ResetStats()
 	FrostboltCount:SetText(string.format(zeroPadString,totalFrostbolts))
 end
 
--- Turn on zero padding if not enabled
--- (adds extra zeros to the left)
-function ZeroPadEnable()
-	if (zeroPadString ~= "%09d") then
-		zeroPadString = "%09d"
-	end
-	FrostboltCount:SetText(string.format(zeroPadString,totalFrostbolts))
+function SetMeterFont(fontName)
+	FrostboltCount:SetFontObject(fontName)
+	preferredFont = fontName
 end
 
--- Turn off zero padding 
-function ZeroPadDisable()
-	if (zeroPadString == "%09d") then
-		zeroPadString = "%d"
-	end
-	FrostboltCount:SetText(string.format(zeroPadString,totalFrostbolts))
-end
-
-
-function FrostboltMeter_OnEnter(self)
-	FrostboltMeterMainReset:Show()
-	FrostboltMeterMainZeroPad:Show()
-end
-
-function FrostboltMeter_OnLeave(self)
-	FrostboltMeterMainReset:Hide()
-	FrostboltMeterMainZeroPad:Hide()
-end
-
+--Event Handler
+--Entry point to handle all events registered for in OnLoad func
 function FrostboltMeterMain_OnEvent(self, event, ...)
 	-- print("Event: "..event)
-	
+
+	-- Make sure saved variables and preferences are loaded
 	if ( event == "PLAYER_ENTERING_WORLD") then
-		--print("Loading Variable: Total Frostbolts: "..totalFrostbolts)
-		FrostboltCount:SetText(string.format(zeroPadString,totalFrostbolts))
-	end
-
-	if (event == "UNIT_SPELLCAST_SUCCEEDED") then
-		local arg1, arg2, arg3 = ...;
-		local spellington = GetSpellInfo(arg3)
-		
-		-- print("SpellID: "..arg3)
-		-- print("Spell Name: "..spellington)
-		
-		if (string.find(spellington, "Frostbolt")) then
-			-- print("Yup we got a bolt: "..spellington)
-			-- print(string.format("%09d", totalFrostbolts))
-			totalFrostbolts = totalFrostbolts + 1
-			FrostboltCount:SetText(string.format(zeroPadString,totalFrostbolts))
-
-			for i,value in pairs(frostyAchieveNums) do
-				print("bolts: "..totalFrostbolts.." table: "..value)
-				if (value == totalFrostbolts) then
-					fire_achievement(totalFrostbolts)
-				end
-			end
+		--Configure from Saved Variables
+		SetMeterFont(preferredFont)
+		if (achievePopUpEnable == true) then
+			FrostboltMeterMainConfigureToggleAchievesHeader:SetText("Toggle Achievement Popups: Enabled")
+		else
+			FrostboltMeterMainConfigureToggleAchievesHeader:SetText("Toggle Achievement Popups: Disabled")
 		end
+			FrostboltCount:SetText(string.format(zeroPadString,totalFrostbolts))
+		end
+
+	-- Parse Combat Events
+	-- This is literally everything from the combat logging
+	-- So we have to parse a bit to get to the info we want
+	if (event == "COMBAT_LOG_EVENT_UNFILTERED") then
+		-- Get 12 baseline payload values
+		-- Only really need sourceName and eventType to get to next block
+		local timestamp, eventType, hideCaster, sourceGUID, sourceName, sourceFlags, sourceFlags2, 
+			  destGUID, destName, destFlags, destFlags2 = CombatLogGetCurrentEventInfo()
+		
+		-- Only care about our own spells, not NPC or other players
+		if (sourceName == UnitName("player")) then
+			-- If it was succesful spell cast event (cast bar finished)
+			if (eventType == "SPELL_CAST_SUCCESS") then
+				local  _,_,_,_,_,_,_,_,_,_,_, spellID, spellName, magSchool = CombatLogGetCurrentEventInfo()
+				print(spellName)
+
+				-- If the spell was a frost bolt
+				if (string.find(spellName, "Frostbolt")) then
+					-- Increment Frostbolts. Set text on frame
+					totalFrostbolts = totalFrostbolts + 1
+					FrostboltCount:SetText(string.format(zeroPadString,totalFrostbolts))
+					-- Check table of achieves in case one needs firing
+					for i,value in pairs(frostyAchieveNums) do
+						if (value == totalFrostbolts) then
+							fire_achievement(totalFrostbolts)
+						end
+					end
+				end
+
+			-- If the spell landed, or partial resisted, or was absorbed
+			elseif (eventType == "SPELL_DAMAGE") then
+				print("spell dmg codeblock")
+				local  _,_,_,_,_,_,_,_,_,_,_, spellID, spellName, magSchool, amount, overkill, schoolAgain, 
+					   wasResisted, wasBlocked, wasAbsorbed, wasCrit = CombatLogGetCurrentEventInfo()
+				if (string.find(spellName, "Frostbolt") and wasCrit == true) then
+					print("Frostbolt Crit!")
+					-- Do stuff
+				end
+			-- Full Resist
+			elseif (eventType == "SPELL_MISSED") then
+				-- Save the miss
+				print("Full Resist")
+			end
+		end 
+		
 	end
 end
 
-function fire_achievement(num_bolts)
-	AchieveTitleString:SetText(num_bolts.." Frostbolts!")
-	AchieveText:SetText(frostyAchieveText[num_bolts])
-	FrostboltMeterMainAchievement:Show()
-	PlaySoundFile("Interface\\AddOns\\FrostboltMeter\\sounds\\fbm_boom_crash_thunder.ogg", "Master")
+function fire_achievement(numBolts)
+	if (achievePopUpEnable == true) then
+		AchieveTitleString:SetText(numBolts.." Frostbolts!")
+		AchieveText:SetText(frostyAchieveText[numBolts])
+		FrostboltMeterMainAchievement:Show()
+		PlaySoundFile("Interface\\AddOns\\FrostboltMeter\\sounds\\fbm_boom_crash_thunder.ogg", "Master")
+	end
 end
 
----------Functions to move the window around
+--====================================================================================
+--		Functions for Window Manipulation
+--====================================================================================
 function FrosboltMeterStartMove(self)
   self:StartMoving();
 end
 function FrostBoltMeterStopMove(self)
   self:StopMovingOrSizing(); 
 end
---FrostboltMeterMain
+
+function FrostboltMeter_OnEnter(self)
+	FrostboltMeterMainConfigButton:Show()
+end
+
+function FrostboltMeter_OnLeave(self)
+	FrostboltMeterMainConfigButton:Hide()
+end
--- FrostboltMeterAchieves.lua
+++ FrostboltMeterAchieves.lua
@@ -1,4 +1,4 @@
--- 23 MAR 2020
+-- 04 APR 2020
 -- Olgruff wants some action in his meter
 -- Olgruff wants noises and pictures and RP
 
@@ -6,20 +6,25 @@
 frostyAchieveText = {}
 frostbolterName = UnitName("player")
 
-frostyAchieveText[100] = 
-"Congratulations Young Mage. You have cast your first one hundred frostbolts."..
-" Your mouth must be parched from all that chanting. But, never fear, you may always conjure more."..
-" Now, do you have it in you to pursue the path of frost?"..
-" Or are you more akin to blazing hands or arcane power?"
+frostyAchieveText[1000] = 
+"Congratulations Young Mage. You have cast your first one hundred frostbolts. "..
+"Your mouth must be parched from all that chanting. But, never fear, you may always conjure some water. "..
+"Now, do you have it in you to pursue the path of frost? "..
+"You may now select different fonts from the menu."
 
-frostyAchieveText[500] = 
-"A five-fold increase since your last casting milestone. You should feel quite good about that, "..frostbolterName.."."..
-" Many mages give up with little weak fingertips by now. "..
+frostyAchieveText[2000] = 
+"A five-fold increase since your last casting milestone. You should feel quite good about that, "..frostbolterName..". "..
+"Many mages give up with little weak fingertips by now. "..
 "It appears that you have a will as hard as impenetrable northrend sea-ice!"
 
-frostyAchieveText[1000] = 
-"ONE ... THOUSAND!? By Medivh's beard, fellow wizard, that is some number."..
-" I see you have quite the affinity for the frozen arts, and we can sure use magi like you."..
-" Rumor has it there are creatures of fire and stone in the depths of Blackrock Mountain".. 
-" only to be destroyed by the frozen heart of a caster such as yourself, "..frostbolterName.."."
+frostyAchieveText[4000] = 
+"ONE ... THOUSAND!? By Medivh's beard, fellow wizard, that is some number. "..
+"I see you have quite the affinity for the frozen arts, and we can sure use magi like you. "..
+"Rumor has it there are creatures of fire and stone in the depths of Blackrock Mountain ".. 
+"only to be destroyed by the frozen heart of a caster such as yourself, "..frostbolterName.."."
 
+frostyAchieveText[8000] = "8000. You must have ice in your veins"
+
+frostyAchieveText[16000] = "16k. You're the price of a small car"
+
+frostyAchieveText[32000] = "Your prowess has grown beyond comprehension. 32k" 
